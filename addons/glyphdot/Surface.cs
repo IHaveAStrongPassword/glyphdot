ae8817b4b2778562b3736721c74e5d6d819a0ff7
IHaveAStrongPassword <achen115@ucr.edu>
2024-03-28 19:43:43 -0700

    add setget

--- addons/glyphdot/Surface.cs
+++ addons/glyphdot/Surface.cs
@@ -9,31 +9,50 @@ using System.Linq;
 [GlobalClass]
 public partial class Surface : Node2D
 {
-	[Export] public Resource font = ResourceLoader.Load("res://addons/glyphdot/IBMCGA+Sharp.tres");
-	private SurfaceFont _font => font is SurfaceFont sf ? sf : null;
-	[Export] public int GridWidth = 1, GridHeight = 1;
-	public Vector2I GridSize => new(GridWidth, GridHeight);
-	public int GlyphCount => GridWidth * GridHeight;
-	public Vector2I GlyphSize => _font.GlyphSize;
-	public Vector2I CanvasSize => GridSize * _font.GlyphSize;
-	public Vector2 ScaledSize => CanvasSize * Scale;
-	public record PrintRect(Rect2I src, Color fore, Color back);
-	private PrintRect[] grid = new PrintRect[1];
+	[Export] public Resource
+		font = ResourceLoader.Load("res://addons/glyphdot/IBMCGA+Sharp.tres");
+	[Export] public int
+		GridWidth = 1,
+		GridHeight = 1;
+	[Export] Color
+		fore = Colors.White,
+		back = Colors.Black;
+	private SurfaceFont _font =>	(SurfaceFont) font;
+	public Vector2I GlyphSize =>	_font.GlyphSize;
+	public int GlyphCount =>		GridWidth * GridHeight;
+	public Vector2I GridSize =>		new(GridWidth, GridHeight);
+	public Vector2I CanvasSize =>	GridSize * _font.GlyphSize;
+	public Vector2 ScaledSize =>	CanvasSize * Scale;
+	private Rect2I empty => _font.GetSrc(' ');
+	private interface ICell {
+		public void Draw (Node2D Surface, SurfaceFont font, Vector2I pos);
+	}
+	public record ColorCell (Color back) : ICell {
+		public void Draw (Node2D Surface, SurfaceFont font, Vector2I pos) {
+			var GlyphSize = font.GlyphSize;
+			Surface.DrawRect(new Rect2I(pos * GlyphSize, GlyphSize), back);
+		}
+	}
+	private record GlyphCell (Rect2I src, Color fore, Color back, char glyph) : ICell {
 
-	[Export] Color fore = Colors.White, back = Colors.Black;
+		public static readonly GlyphCell empty = new GlyphCell(new Rect2I(), Colors.Transparent, Colors.Transparent, ' ');
+		public void Draw(Node2D Surface, SurfaceFont font, Vector2I pos) {
+			var GlyphSize = font.GlyphSize;
+			Surface.DrawRect(new Rect2I(pos * GlyphSize, GlyphSize), back);
+			Surface.DrawTextureRectRegion(font.Texture, new Rect2I(pos * GlyphSize, GlyphSize), src, fore);
+		}
+	};
+	private GlyphCell[] grid = new GlyphCell[1];
 	public override void _Ready() {
 		ResetGrid();
 		//Debug.Print($"{Resource.IsInstanceValid(_font)} {_font}");
 	}
+	public void ResetGrid () => grid = new GlyphCell[GlyphCount];
 	public void SetSize(int w, int h) {
 		GridWidth = w;
 		GridHeight = h;
 		ResetGrid();
 	}
-	public void ResetGrid () {
-		grid = new PrintRect[GlyphCount];
-	}
-
 	public override void _Process(double delta){
 		base._Process(delta);
 		if (Engine.IsEditorHint()){
@@ -47,25 +66,16 @@ public partial class Surface : Node2D
 			DrawRect(new(Transform.Origin, GridSize * GlyphSize), back, true);
 			return;
 		}
-        foreach (var (index, rect) in grid.Select((rect, index) => (index, rect))) {
-			int x = index % GridWidth,
-				y = index / GridWidth;
-			var pos = new Vector2I(x, y);
-			if(rect == null) {
-				continue;
-			}
-			DrawRect(new Rect2I(pos * GlyphSize, GlyphSize), rect.back);
-			DrawTextureRectRegion(_font.Texture, new Rect2I(pos * GlyphSize, GlyphSize), rect.src, rect.fore);
-        };
-	}
-	Rect2I empty => _font.GetSrc(' ');
-	public void Clear(){
-		foreach(var(index, rect) in grid.Select((rect, index) => (index, rect))){
-			grid[index] = new PrintRect(empty, fore, back);
+		foreach(var index in Enumerable.Range(0, grid.Length)) {
+			if(grid[index] is not { }rect) continue;
+			rect.Draw(this, _font, IndexToPos(index));
 		}
 	}
-	public void Print(int x, int y, string s)=>
-		Print(x, y, s, fore, back);
+	public void Clear(){
+		foreach(var index in Enumerable.Range(0, grid.Length))
+			grid[index] = null;
+	}
+	public void Print(int x, int y, string s) => Print(x, y, s, fore, back);
 	public void Print(int x, int y, string s, Color f, Color b){
 		foreach(var c in s){
 			Print(x, y, c, f, b);
@@ -75,17 +85,34 @@ public partial class Surface : Node2D
 			}
 		}
 	}
-	public void Print(int x, int y, char c)
-	=>
-		Print(x, y, c, fore, back);
+	public void Print(int x, int y, char c) => Print(x, y, c, fore, back);
 	public void Print(int x, int y, char c, Color f, Color b){
 		//Debug.Print($"{grid.Length} {x} {y} {Resource.IsInstanceValid(_font)} {_font}");
-		grid[y * GridWidth + x] = new PrintRect(_font.GetSrc(c), f, b);
+		grid[PosToIndex(x, y)] =
+			new GlyphCell(_font.GetSrc(c), f, b, c);
 	}
 
-	
-}
+	public Vector2I IndexToPos (int index) => new Vector2I(index % GridWidth, index / GridWidth);
+	public int PosToIndex (int x, int y) => y * GridWidth + x;
+	public Color GetCellForeground (int x, int y) => grid[PosToIndex(x, y)].fore;
+	public Color GetCellBackground (int x, int y) => grid[PosToIndex(x, y)].back;
+	public char GetGlyph (int x, int y) => grid[PosToIndex(x, y)].glyph;
 
+	public void SetCellForeground(int x, int y, Color foreground) {
+		var index = PosToIndex(x, y);
+		grid[index] = (grid[index] ?? GlyphCell.empty) with { fore = foreground };
+	}
+	public void SetCellBackground(int x, int y, Color background) {
+		var index = PosToIndex(x, y);
+		grid[index] = (grid[index] ?? GlyphCell.empty) with { back = background };
+	}
+	public void SetGlyph (int x, int y, char c) {
+		var index = PosToIndex(x, y);
+		grid[index] = (grid[index] ?? GlyphCell.empty) with { glyph = c };
+	}
+
+
+}
 public static class SSurface {
 	public static void FullScreen (this Surface s) {
 		var ws = DisplayServer.WindowGetSize();
